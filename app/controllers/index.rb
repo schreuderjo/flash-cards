c2cb1f77ab6c470c74d3958ec3112fddb0f9fd04
John Williams & Jordan Schreuder <pair+johnw335.schreuderjo@devbootcamp.com>
2014-10-31 16:13:20 -0500

    adds migrations, models and seeds

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,34 +3,3 @@ get '/' do
   erb :index
 end
 
-get '/new_user' do
-  erb :'/new'
-end
-
-post '/' do
-  #find the user with the params given
-  #call authenticate method
-  #if a user is found
-    #go to the secret page
-  #else
-    #redirect to the login
-    #maybe throw an error
-  # value = rand(2)
-  if User.authenticate(params[:user])
-    erb :'/secret'
-  else
-    redirect '/'
-
-  end
-end
-
-post '/home' do
-  "This is the website!"
-  User.create!(params[:user])
- redirect '/home'
-end
-
-get '/home' do
-  @users = User.all
-  erb :'/home'
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
-  def self.authenticate (attributes)
-    # if self.
-    user = User.find_by(email: attributes[:email], password: attributes[:password])
+  validates :email, uniqueness: true
+  validates :password_hash, uniqueness: true
+
+  has_many :rounds
+  has_many :decks, through: :rounds
+
+  def self.authenticate(attributes)
+    User.find_by(email: attributes[:email], password_hash: attributes[:password])
   end
+
+  # def password
+  #   @password ||= BCrypt::Password.new(password_hash)
+  # end
+
+  # def password=(new_password)
+  #   @password = BCrypt::Password.create(new_password)
+  #   self.password_hash = @password
+  # end
+
 end
