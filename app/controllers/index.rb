542a1511bd45218e8c1956a0dd0d813e83b5dd0f
Jordan Schreuder <schreuderjo@gmail.com>
2014-11-01 11:42:14 -0500

    some basic card passing functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -46,15 +46,23 @@ end
 get '/decks/:deck_id' do
   deck_id = params[:deck_id]
   @deck = Deck.find(deck_id)
-  card = @deck.cards.find(52)
+  card = @deck.cards.sample
   card_id = card.id
   redirect "/decks/#{deck_id}/cards/#{card_id}"
 end
 
 get '/decks/:deck_id/cards/:card_id' do
   @card = Card.find(params[:card_id])
+  @deck_id = params[:deck_id]
   erb :"cards/show"
 end
+
+post '/decks/:deck_id/cards/:card_id' do
+  @guess = Guess.create!(params[:user])
+  raise @guess.inspect
+  "Hello World"
+end
+
 
 
 
